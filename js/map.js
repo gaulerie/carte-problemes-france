eac82b82719fcb6e167e949dbbb661a8116bf5c1
gaulerie <emilien.courvoisier@gmail.com>
2024-04-14 09:22:10 +0900

    d

--- js/map.js
+++ js/map.js
@@ -130,3 +130,19 @@ polygonData.on("click", function (e) {
 pointData.on("click", function (e) {
   $("#sidebar").addClass("displayed");
 });
+
+// Écouteur d'événement pour la fermeture de la popup
+map.on("popupclose", function (e) {
+  $("#sidebar").removeClass("displayed");
+});
+
+// Gestionnaire d'événement pour le bouton croix de la popup
+$("#popup-close-btn").click(function () {
+  map.closePopup();
+  $("#sidebar").removeClass("displayed");
+});
+
+// Gestionnaire d'événement pour le bouton croix de la sidebar
+$("#sidebar-close-btn").click(function () {
+  $("#sidebar").removeClass("displayed");
+});
