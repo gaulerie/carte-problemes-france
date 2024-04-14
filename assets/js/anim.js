1142fd43a7217f6e391ef0d1b901a435beaaed22
gaulerie <emilien.courvoisier@gmail.com>
2024-04-14 12:42:22 +0900

    0.001

--- assets/js/anim.js
+++ assets/js/anim.js
@@ -1,25 +1,30 @@
 // Pour les couches polygonales
 polygonData.on("click", function (e) {
   $("#sidebar").addClass("displayed");
+  $(".leaflet-control-zoom").addClass("swiped");
 });
 
 // Pour les points
 pointData.on("click", function (e) {
   $("#sidebar").addClass("displayed");
+  $(".leaflet-control-zoom").addClass("swiped");
 });
 
 // Écouteur d'événement pour la fermeture de la popup
 map.on("popupclose", function (e) {
   $("#sidebar").removeClass("displayed");
+  $(".leaflet-control-zoom").removeClass("swiped");
 });
 
 // Gestionnaire d'événement pour le bouton croix de la popup
 $("#popup-close-btn").click(function () {
   map.closePopup();
   $("#sidebar").removeClass("displayed");
+  $(".leaflet-control-zoom").removeClass("swiped");
 });
 
 // Gestionnaire d'événement pour le bouton croix de la sidebar
 $("#sidebar-close-btn").click(function () {
   $("#sidebar").removeClass("displayed");
+  $(".leaflet-control-zoom").removeClass("swiped");
 });
--- assets/js/map.js
+++ assets/js/map.js
@@ -24,7 +24,6 @@ googleSat = L.tileLayer("http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}", {
   subdomains: ["mt0", "mt1", "mt2", "mt3"],
 });
 
-// Mettre ci-dessous la map qu'on veut par défaut
 googleStreets.addTo(map);
 
 // var wms = L.tileLayer.wms("http://localhost:8080/geoserver/wms", {
@@ -36,9 +35,63 @@ googleStreets.addTo(map);
 
 // GeoJSON
 
+var pointJson = {
+  type: "FeatureCollection",
+  features: [
+    { type: "Feature", properties: { id: 1 }, geometry: { type: "Point", coordinates: [4.8357, 45.764] } },
+    { type: "Feature", properties: { id: 2 }, geometry: { type: "Point", coordinates: [2.3522, 48.8566] } },
+  ],
+};
+
+var pointData; // Variable pour stocker les données GeoJSON
+
+// Envoyer une requête GET à votre Web App
+fetch("https://script.google.com/macros/s/AKfycbyBCceivdsdI5utTYqA1gYBEiP78OBkjGs9ZTDNL6G7yGQIWsx1HqGQ6yi8uoa79RA1/exec")
+  .then((response) => response.json()) // Convertir la réponse en JSON
+  .then((data) => {
+    pointData = data;
+  })
+  .catch((error) => {
+    console.error("Erreur lors de la récupération des données :", error);
+  });
+
+function updateSidebar(data, index) {
+  // Vérifier si l'index spécifié est valide
+  if (index >= 0 && index < data.length) {
+    $(".surname").text(data[index]["Nom"]);
+    $(".name").text(data[index]["Prénom"]);
+    $(".birthdate").text(data[index]["Date de Naissance"]);
+    $(".countryEntranceDate").text(data[index]["Date d'entrée en France"]);
+    $(".nationalities").text(data[index]["Nationalités"]);
+    $(".ethnicities").text(data[index]["Ethnicités"]);
+
+    for (let i = 0; i < 3; i++) {
+      $(".actDate")
+        .eq(i)
+        .text(data[index]["Acte " + (i + 1) + " - Date"]);
+      $(".actTitle")
+        .eq(i)
+        .text(data[index]["Acte " + (i + 1) + " - Titre"]);
+      $(".actDescription")
+        .eq(i)
+        .text(data[index]["Acte " + (i + 1) + " - Description"]);
+    }
+
+    $(".street").text(data[index]["Rue"]);
+    $(".region").text(data[index]["Région"]);
+    $(".city").text(data[index]["Ville"]);
+  } else {
+    console.error("Index spécifié invalide.");
+  }
+}
+
 var pointData = L.geoJSON(pointJson, {
   onEachFeature: function (feature, layer) {
     layer.bindPopup(`<b>Name: </b>` + feature.properties.name);
+    layer.on("click", function () {
+      console.log("Index du point:", feature.properties.id);
+      updateSidebar(pointData, feature.properties.id - 1);
+    });
   },
   style: {
     fillColor: "#fff",
@@ -46,9 +99,14 @@ var pointData = L.geoJSON(pointJson, {
     color: "#fff",
   },
 }).addTo(map);
+
 var polygonData = L.geoJSON(polygonJson, {
   onEachFeature: function (feature, layer) {
     layer.bindPopup(`<b>Name: </b>` + feature.properties.name);
+    layer.on("click", function () {
+      console.log("Index du point:", feature.properties.id);
+      updateSidebar(pointData, feature.properties.id - 1);
+    });
   },
   style: {
     fillColor: "#fff",
@@ -68,11 +126,23 @@ var baseMaps = {
 
 var overlayMaps = {
   Points: pointData,
-  Zones: polygonData,
+  // Zones: polygonData,
   // wms: wms,
 };
 
 L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map);
+
+// Marker Style
+var greenIcon = L.icon({
+  iconUrl: "leaf-green.png",
+  shadowUrl: "leaf-shadow.png",
+
+  iconSize: [38, 95],
+  shadowSize: [50, 64],
+  iconAnchor: [22, 94],
+  shadowAnchor: [4, 62],
+  popupAnchor: [-3, -76],
+});
 
 // Events
 
