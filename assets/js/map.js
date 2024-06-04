2b98514fb5fedbce24e783aed888f1522a95c8a6
gaulerie <emilien.courvoisier@gmail.com>
2024-06-04 10:00:20 +0900

    Fix

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,26 +1,4 @@
-var map = L.map("map", {
-  center: [37, 138],
-  zoom: 5,
-  zoomAnimation: true,
-  fadeAnimation: true,
-  markerZoomAnimation: true,
-  zoomAnimationThreshold: true,
-  animate: true,
-  // renderer: L.canvas(),
-});
-
-// Fonction pour créer une info-bulle pour les marqueurs
-function createMarkerTooltip(marker, title) {
-  if (map.getZoom() > 7) {
-    var tooltip = L.tooltip({
-      direction: "right",
-      permanent: true,
-      className: "marker-tooltip",
-    }).setContent(title);
-
-    marker.bindTooltip(tooltip).openTooltip();
-  }
-}
+var map = L.map("map").setView([47, 5], 6);
 
 // GeoJSON vide initial
 var pointJson = {
@@ -67,114 +45,105 @@ var pointJson = {
   features: [],
 };
 
-// Marker Cluster Group
-
-var markers = L.markerClusterGroup({
-  disableClusteringAtZoom: 8,
-  iconCreateFunction: function (cluster) {
-    var count = cluster.getChildCount();
-    var size = count < 100 ? "small" : count < 1000 ? "medium" : "large";
-    var color = count < 10 ? "green" : count < 100 ? "yellow" : count < 1000 ? "orange" : "red";
-    return L.divIcon({
-      html: "<div numberColor=" + color + ' class="circle-marker">' + count + "</div>",
-      className: "cluster-icon-" + size,
-      iconSize: [30, 30],
-    });
-  },
-  showCoverageOnHover: false,
-});
-
-var globalData;
-
-fetch("https://script.google.com/macros/s/AKfycbzRqRi_s1oWswewaOgwCEiH6izX5y-3yCARwe1BZIqfmX5qC6VA_vpDchgYjF95FgdgIQ/exec")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("La requête a échoué avec le statut " + response.status);
-    }
-    return response.json();
-  })
+fetch("https://script.google.com/macros/s/AKfycbyBCceivdsdI5utTYqA1gYBEiP78OBkjGs9ZTDNL6G7yGQIWsx1HqGQ6yi8uoa79RA1/exec")
+  .then((response) => response.json())
   .then((data) => {
-    globalData = data; // Stocker les données globalement
-
-    // Ajouter des marqueurs à la Marker Cluster Group
+    var pointFeatures = [];
     data.forEach((line, index) => {
       var latitude = parseFloat(line["Latitude"]);
       var longitude = parseFloat(line["Longitude"]);
-      var title = line["Title"]; // Récupérer le titre du marqueur
-      if (!isNaN(latitude) && !isNaN(longitude)) {
-        var iconUrl = "";
-        var type = line["Type"]; // Récupérer le type depuis la colonne "Type"
-        if (type === "Photo Spot") {
-          iconUrl = "assets/img/togo.png";
-        } else if (type === "Hall of Fame") {
-          iconUrl = "assets/img/star.png";
-        }
-        var customIcon = L.icon({
-          iconUrl: iconUrl,
-          iconSize: [20, 20], // Taille de l'icône
-          iconAnchor: [10, 10], // Point d'ancrage de l'icône
-        });
-        var marker = L.marker([latitude, longitude], { id: index, icon: customIcon }); // Utiliser l'icône personnalisée
-        marker.bindTooltip(title, { permanent: false, direction: "right", offset: [20, 0] }); // Ajouter l'infobulle avec le titre du marqueur
-        markers.addLayer(marker);
-      }
+      var newFeature = {
+        type: "Feature",
+        properties: { id: index },
+        geometry: { type: "Point", coordinates: [longitude, latitude] },
+      };
+      pointFeatures.push(newFeature);
     });
+    var pointGeoJSON = {
+      type: "FeatureCollection",
+      features: pointFeatures,
+    };
+    pointData = L.geoJSON(pointGeoJSON, {
+      onEachFeature: function (feature, layer) {
+        // layer.bindPopup(`<p>Name: </p>` + feature.properties.id);
+        layer.on("click", function () {
+          updateSidebar(data, feature.properties.id);
+          $("#sidebar").addClass("displayed");
+          $(".leaflet-control-zoom").addClass("swiped");
+        });
+      },
+    }).addTo(map);
 
-    map.addLayer(markers);
-    updateMarkerText(); // Initialiser l'affichage du texte
+    polygonData = L.geoJSON(polygonJson, {
+      onEachFeature: function (feature, layer) {
+        // layer.bindPopup(`<p>Name: </p>` + feature.properties.id);
+        layer.on("click", function () {
+          updateSidebar(pointData, feature.properties.id);
+          $("#sidebar").addClass("displayed");
+          $(".leaflet-control-zoom").addClass("swiped");
+        });
+      },
+    }).addTo(map);
   })
   .catch((error) => {
     console.error("Erreur lors de la récupération des données :", error);
   });
 
-map.on("zoomend", function () {
-  updateMarkerText();
-});
+function updateSidebar(data, index) {
+  if (index >= 0 && index < data.length) {
+    $(".surname").text(data[index]["Nom"]);
+    $(".name").text(data[index]["Prénom"]);
+    $(".birthdate").text(data[index]["Date de Naissance"]);
+    $(".countryEntranceDate").text(data[index]["Date d'entrée en France"]);
+    $(".nationalities").text(data[index]["Nationalités"]);
+    $(".ethnicities").text(data[index]["Ethnicités"]);
 
-function updateMarkerText() {
-  if (map.getZoom() > 11) {
-    markers.eachLayer(function (layer) {
-      var title = globalData[layer.options.id]["Title"]; // Récupérer le titre du marqueur
-      if (!layer.getTooltip()) {
-        layer.bindTooltip(title, { permanent: true, direction: "right", offset: [20, 0] });
-      }
-      layer.openTooltip();
-    });
+    for (let i = 0; i < 3; i++) {
+      $(".actDate")
+        .eq(i)
+        .text(data[index]["Acte " + (i + 1) + " - Date"]);
+      $(".actTitle")
+        .eq(i)
+        .text(data[index]["Acte " + (i + 1) + " - Titre"]);
+      $(".actDescription")
+        .eq(i)
+        .text(data[index]["Acte " + (i + 1) + " - Description"]);
+    }
+
+    $(".street").text(data[index]["Rue"]);
+    $(".latitude").text(data[index]["Latitude"]);
+    $(".longitude").text(data[index]["Longitude"]);
+    $(".personType").text(data[index]["Type"]);
+    // $(".region").text(data[index]["Région"]);
+    $(".city").text(data[index]["Ville"]);
+    // $(".sidebarimg").css((background = "url(" + data[index]["Image"] + ");");
+    $(".sidebarimg").css("background-image", "url(" + data[index]["Image"] + ")");
   } else {
-    markers.eachLayer(function (layer) {
-      layer.unbindTooltip();
-    });
+    console.error("Index spécifié invalide. Index:", index);
   }
 }
 
-markers.on("click", function (event) {
-  var layer = event.layer;
-  var id = layer.options.id; // Récupérer l'ID unique du marqueur
-  var properties = findPropertiesByIndex(id, globalData); // Trouver les propriétés associées à l'index
-  updateSidebar(properties);
-  $("#sidebar").addClass("displayed");
-  // $(".leaflet-control-zoom").addClass("swiped");
-});
+var pointData = L.geoJSON(pointJson, {
+  onEachFeature: function (feature, layer) {
+    // layer.bindPopup(`<p>Name: </p>` + feature.properties.id);
+    layer.on("click", function () {
+      updateSidebar(pointData, feature.properties.id);
+      $("#sidebar").addClass("displayed");
+      $(".leaflet-control-zoom").addClass("swiped");
+    });
+  },
+}).addTo(map);
 
-function findPropertiesByIndex(index, data) {
-  return data[index] ? data[index] : null;
-}
-
-function updateSidebar(properties) {
-  if (properties) {
-    $(".labelType").text(properties["Type"]);
-    $(".title").text(properties["Title"]);
-    $(".note").text(properties["Note"]);
-    $(".googleMapsLink").text(properties["Link"]);
-    $(".prefecture").text(properties["Préfecture"]) + " Prefecture";
-    $(".id").text("Numéro de ligne : " + globalData.indexOf(properties));
-    $(".latitude").text(properties["Latitude"]);
-    $(".longitude").text(properties["Longitude"]);
-    $(".sidebarimg").css("background-image", "url(" + properties["Image"] + ")");
-  } else {
-    console.error("Propriétés non valides.");
-  }
-}
+var polygonData = L.geoJSON(polygonJson, {
+  onEachFeature: function (feature, layer) {
+    // layer.bindPopup(`<p>Name: </p>` + feature.properties.id);
+    layer.on("click", function () {
+      updateSidebar(pointData, feature.properties.id);
+      $("#sidebar").addClass("displayed");
+      $(".leaflet-control-zoom").addClass("swiped");
+    });
+  },
+}).addTo(map);
 
 // Controls
 
@@ -186,7 +155,6 @@ var baseMaps = {
 };
 
 var overlayMaps = {
-  Markers: markers,
   // Points: pointData,
   // Zones: polygonData,
   // wms: wms,
@@ -204,8 +172,3 @@ L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map);
 //   document.getElementsByClassName("coordinate")[0].innerHTML = "lat: " + e.latlng.lat + "lng: " + e.latlng.lng;
 // console.log("lat: " + e.latlng.lat, "lng: " + e.latlng.lng);
 // });
-
-// Événement de carte pour mettre à jour les marqueurs visibles
-map.on("moveend", function () {
-  markers.refreshClusters();
-});
